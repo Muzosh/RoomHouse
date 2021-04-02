acbe828d19a07509582ba4294b91834cea057376
Slavka <slavkadz@gmail.com>
2021-04-02 19:23:53 +0200

    Self-video

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+import os
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant
+
+load_dotenv()
+twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+twilio_api_key_sid = os.environ.get('TWILIO_API_KEY_SID')
+twilio_api_key_secret = os.environ.get('TWILIO_API_KEY_SECRET')
 
 app = Flask(__name__)
 
